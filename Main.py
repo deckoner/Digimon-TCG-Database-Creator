4ce82a6d9311732667fb4e6b383eb238ae2a7ba7
deckoner <deckonerDev@gmail.com>
2025-04-09 13:59:53 +0200

    Minor correction

--- Main.py
+++ Main.py
@@ -235,6 +235,8 @@ def create_db_structure():
     """
     Creates all tables and relationships in the database.
     """
+    _create_database_if_not_exists()
+
     connection = _create_connection()
     cursor = connection.cursor()
 
@@ -376,8 +378,7 @@ def fill_db():
     """
     Stuff the database with card data from the CSV file.
     """
-    _create_database_if_not_exists()
-
+    
     connection = _create_connection()
     cursor = connection.cursor()
 
@@ -730,7 +731,7 @@ def _download_convert_image(url, filename):
 
 
 if __name__ == "__main__":
-    create_csv()
+    # create_csv()
     create_db_structure()
     fill_db()
     # update_db()
